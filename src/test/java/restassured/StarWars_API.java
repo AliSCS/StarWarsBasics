1a08cb3bb8f6c5d553a7aadc7290d495f084f389
Ali Al-Marsumi <Ali Al-Marsumi>
2017-09-17 19:24:37 +0100

    test files first commit

---
+++ src/test/java/restassured/StarWars_API.java
@@ -0,0 +1,48 @@
+package restassured;
+
+
+
+import io.restassured.RestAssured;
+import org.junit.Test;
+import org.junit.Before;
+import io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import io.restassured.path.json.JsonPath;
+import org.junit.Assert;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.expect;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
+/**
+ * Created by Ali Al-Marsumi Local on 13/09/2017.
+ */
+public class StarWars_API {
+
+    @Before
+    public void setup(){
+        RestAssured.baseURI= "http://swapi.co/api/";
+    }
+
+
+    @Test
+    public void canGetName(){
+        expect().body("name",equalTo("Luke Skywalker")).when().get("http://swapi.co/api/people/1/");
+
+    }
+
+    @Test
+    public void canGetFilms(){
+        RestAssured.get("people/1/").then().assertThat().body("films[0]", equalTo("http://swapi.co/api/films/2/"));
+    }
+
+
+    @Test
+    public void canGetMass(){
+        given().queryParam("name", "Anakin Skywalker").when().get("people/11").then().body("mass", equalTo("84"));
+    }
+}
+
